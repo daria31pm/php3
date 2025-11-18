67e2de4eba9a1b750a701a5517b49e467e580494
даша <dmarueva1234@yandex.ru>
2025-11-18 16:08:33 +0300

    for.php

---
+++ for.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Цикл while</title>
+</head>
+<body>
+    <h1>Цикл while</h1>
+    <?php
+    /*
+    ЗАДАНИЕ
+    - Создайте переменную $var и присвойте ей строковое значение 'ПРИВЕТ'
+    - Используя цикл while выведите значение переменной $var в столбик так, 
+      чтобы на выходе в браузере получилось:
+    П
+    Р
+    И
+    В
+    Е
+    Т
+    */
+    
+    $var = 'ПРИВЕТ';
+    $i = 0;
+    while ($i < strlen($var)) {
+        echo $var[$i] . "<br>";
+        $i++;
+    }
+    ?> 
+</body>
+</html>
